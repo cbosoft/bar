0b613112f09cd39619e90ff3f05a467549030233
Chris Boyle <christopher.boyle.101@strath.ac.uk>
2020-04-16 13:42:48 +0100

    Added arg parsing, help

--- src/main.cpp
+++ src/main.cpp
@@ -5,36 +5,64 @@
 
 #define EITHER(A,B) ( (strcmp(argv[i], A) == 0) or (strcmp(argv[i], B) == 0) )
 
+void show_help_and_exit()
+{
+  std::cerr
+   << "  " BOLD "BAR" RESET "\n"
+   << "\n"
+   << "  " BOLD "Usage:" RESET "\n"
+   << "    bar [--recipe <recipe>]\n"
+   << "    bar --initial <val> --subsequent <val> --pattern <val> [--pattern <val> ...]\n"
+   << "\n"
+   ;
+  exit(0);
+}
+
 
 int main(int argc, const char **argv)
 {
+
   std::string initial = ":";
   std::string subsequent = ":";
   std::list<std::string> patterns;
 
   argc--; argv++;
-  for (int i = 0; i < argc; i++) {
+  if (argc) {
+    for (int i = 0; i < argc; i++) {
 
-    if (EITHER("-p", "--pattern")) {
-      std::string s(argv[++i]);
-      patterns.push_back(s);
+      if (EITHER("-p", "--pattern")) {
+        std::string s(argv[++i]);
+        patterns.push_back(s);
+      }
+      else if (EITHER("-i", "--initial")) {
+        initial = std::string(argv[++i]);
+      }
+      else if (EITHER("-s", "--subsequent")) {
+        subsequent = std::string(argv[++i]);
+      }
+      else if (EITHER("-h", "--help")) {
+        show_help_and_exit();
+      }
+      else if (EITHER("-r", "--recipe")) {
+
+        Recipe::from_file(std::string(argv[++i])).monitor();
+        return 0;
+
+      }
+      else {
+        throw ArgumentError(Formatter() << "Unknown argument " << argv[i] << ".");
+      }
+
     }
-    else if (EITHER("-i", "--initial")) {
-      initial = std::string(argv[++i]);
+
+    if (!patterns.size()) {
+      throw ArgumentError(Formatter() << "No files or patterns specified.");
     }
-    else if (EITHER("-s", "--subsequent")) {
-      subsequent = std::string(argv[++i]);
-    }
-    else {
-      throw ArgumentError(Formatter() << "Unknown argument " << argv[i] << ".");
-    }
+
+    Recipe(patterns, initial, subsequent).monitor();
+  }
+  else {
+    Recipe::from_file(".bar_recipe").monitor();
   }
 
-  if (!patterns.size()) {
-    throw ArgumentError(Formatter() << "No files specified to watch.");
-  }
-
-  // TODO read recipe from file
-  Recipe r(patterns, initial, subsequent);
-  r.monitor();
 }
