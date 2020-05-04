1e5546345d00a7ec3729c6ade207d1f1195f6d34
Chris Boyle <christopher.boyle.101@strath.ac.uk>
2020-05-04 14:39:19 +0100

    Improved help spiel

--- src/main.cpp
+++ src/main.cpp
@@ -8,12 +8,28 @@
 void show_help_and_exit()
 {
   std::cerr
-   << "  " BOLD "BAR" RESET "\n"
+   << BOLD "bar" RESET "\n"
    << "\n"
-   << "  " BOLD "Usage:" RESET "\n"
-   << "    bar [--recipe <recipe>]\n"
-   << "    bar --initial <val> --subsequent <val> --pattern <val> [--pattern <val> ...]\n"
+   << "Watches for file changes then runs specified commands.\n"
    << "\n"
+   << BOLD "Usage:" RESET "\n"
+   << "  bar\n"
+   << "\n"
+   << BOLD "Config File" RESET "\n"
+   << "Files and commands are specified by config files. bar looks for a file named\n"
+   << "\".bar_recipe\" in the current directory. The configuration file constists of\n"
+   << "sets of key-value pairs, one pair per line, separated by colon ':'.\n"
+   << "\n"
+   << "Values:\n"
+   << "  command  ::  The shell command that will run when file changes are detected. Required.\n"
+   << "\n"
+   << "  initial  ::  If present, the command that is run when bar starts up. Optional.\n"
+   << "\n"
+   << "  pattern  ::  A glob-pattern specifying the files to monitor. Can be specified\n"
+   << "               any number of times, but at least once.\n"
+   << "\n"
+   << "There can be multiple recipes specified in the same file, separated by three\n"
+   << "dashes: \"---\".\n"
    ;
   exit(0);
 }
